c087e1d1079101475a6ee43f0b68036dfd2bec2c
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-01 00:29:13 +0530

    TYPE CONVERSION

---
+++ BASICS/TypeConversion.js
@@ -0,0 +1,25 @@
+let score = "33"
+console.log(typeof score);
+
+// CONVERSION.....
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+// NOTES:
+// "33" --> 33
+// "33a" --> NaN
+//FOR BOOLEAN VALUES 1 FOR TRUE AND 0 FOR FALSE VALUES .
+
+
+let a = 1
+let boolIsa = Boolean(a);
+console.log(boolIsa);
+// NOTES:
+// 1 --> TRUE &  0 --> FALSE 
+// "" --> FALSE
+// "KARAN" --> FALSE 
+
+let somenumber = 20
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
