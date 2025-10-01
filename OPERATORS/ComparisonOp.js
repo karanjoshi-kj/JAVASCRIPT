e563fc991823a78fa66689b70cd255e9492b9f23
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-01 18:04:12 +0530

    DETAILED EXPLANATION ON OPERATORS

--- OPERATORS/ComparisonOp.js
+++ OPERATORS/ComparisonOp.js
@@ -1,8 +1,20 @@
+//DURING COMPARISON THE COMPILER SOMETIMES NOT GIVES THE PREDECTIVE VALUE :
+
 var a = 5
 var b = "5"
-console.log(a==b); // DOUBLE EQUAL (EQUAL) OPERATOR ONLY CHECKS THE VALUE , NOT TYPE 
-// BEFORE COMPARING , JAVASCRIPT WILL TRY TO CONVERT "10" INTO  A NUMBER .
+console.log(a==b); // DOUBLE EQUAL (==) OPERATOR Returns true if the operands are equal 
+console.log(a===b);
+// STRICT EQUAL (===) Returns true if the operands are equal and of the same type.
 
 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// THE REASON OF THE DIFFRENT OUTPUT IS THE DIFFRENT WORKING OF THE EQUALITY CHECK(==) AND THE COMPARISON(> , < , >= , <=)
+// comparison operator converts null to a number , treating it as 0 ..
+
+// NOTE:
+// UNDEFINED OPERAOTR ALWAYS GIVES FALSE VALUE ON BOTH COMPARISON AND EQUALITY 
 
 
