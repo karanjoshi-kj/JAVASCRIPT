6561f4d00ba924b0309a1dd409b079065d60f642
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-02 12:36:22 +0530

    BASICS OF THE STRING , STRING INTERPOLATION & SOME BASIC METHODS OF THE STRINGS IN JS

---
+++ STRINGS/StringDeclaration.js
@@ -0,0 +1,22 @@
+// STRING IS AN OBJECT OR A KEY-VALUE PAIR IN JS 
+
+// DECLARATION OF THE STRING .. 
+const gamename = new String('karanjoshi-kj');
+console.log(gamename[0]); // ACCESSING THE 0TH KEY ..
+console.log(gamename.__proto__); // ACCESSING THE PROTOTYPE ..
+
+// METHODS IN STRING .. 
+console.log(gamename.length);
+console.log(gamename.toUpperCase()); //NOTE: IT DOES'NT HAVE CHANGE THE ORIGINAL STRING 
+console.log(gamename.charAt(2));  
+console.log(gamename.indexOf('j'));  
+
+const newstring = gamename.substring(0,4) //NOTE : HERE 4 IS NOT INCLUDED MEANS VALUES ARE PRINTED TILL THE THIRD INDEX ONLY :..
+console.log(newstring);
+
+// NOTE : WE CAN TAKE (-)VE VALUES ONLY IN THE SLICE METHOD NOT IN THE SUBSTRING METHOD ....
+const anotherstring = gamename.slice(-9,-3)
+console.log(anotherstring);
+
+
+
---
+++ STRINGS/StringInterpolation.js
@@ -0,0 +1,11 @@
+const name = "karan"
+const repocount = 10
+
+// THIS METHOD TO PRINT IS NOT BEEN USED IN NOW A DAYS 
+console.log(name + repocount + " value");
+
+// FOR THIS WE USE --> 
+
+// STRING INTERPOLATION : IN THIS WE USE BACKTICKS IN WHICH WE MAKE THE PLACEHOLDERS....
+console.log(`HELLO MY NAME IS ${name} AND MY REPO COINT IS ${repocount}`);
+
