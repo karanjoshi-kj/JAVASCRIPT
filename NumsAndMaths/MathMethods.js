eacbcaaa5080331a81403e4ac41a750a2c940a0d
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-03 20:02:03 +0530

    MATH METHODS AND BASICS

--- NumsAndMaths/MathMethods.js
+++ NumsAndMaths/MathMethods.js
@@ -1,15 +1,15 @@
-// // METHODS IN MATH OBJECT .... 
+// METHODS IN MATH OBJECT .... 
 
 
-// console.log(Math.abs(-4)); // ONLY CONVERTS THE NEGATIVE VALUES TO THE POSITIVE VALUES NOT VICE-VERSA.....
-// console.log(Math.round(4.6)); // ROUND OF THE DECIMAL VALUES
+ console.log(Math.abs(-4)); // ONLY CONVERTS THE NEGATIVE VALUES TO THE POSITIVE VALUES NOT VICE-VERSA.....
+ console.log(Math.round(4.6)); // ROUND OF THE DECIMAL VALUES
 
-// console.log(Math.abs(-4)); // ONLY CONVERTS THE NEGATIVE VALUES TO THE POSITIVE VALUES NOT VICE-VERSA.....
-// console.log(Math.ceil(4.6));  // FOR UPPER VALUES..
-// console.log(Math.floor(4.6)); // FOR LOWER VALUES..
+ console.log(Math.abs(-4)); // ONLY CONVERTS THE NEGATIVE VALUES TO THE POSITIVE VALUES NOT VICE-VERSA.....
+ console.log(Math.ceil(4.6));  // FOR UPPER VALUES..
+ console.log(Math.floor(4.6)); // FOR LOWER VALUES..
 
-// console.log(Math.max(5,8,2,14,6)); // MAXIMUM VALUE..
-// console.log(Math.min(5,8,2,14,6)); // MINIMUM VALUE..
+ console.log(Math.max(5,8,2,14,6)); // MAXIMUM VALUE..
+ console.log(Math.min(5,8,2,14,6)); // MINIMUM VALUE..
 
 // IMPORTANT METHOD .. 
 console.log(Math.random()); // THIS SHOWS THAT IT WILL THROW THE VALUE IN BETWEEN 0 TO 1 ALWAYS
