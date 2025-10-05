f135d14d824a7b3900e8baeae975892f285e2e02
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-05 14:21:46 +0530

    DATE & TIME

---
+++ DateAndTime/Date.js
@@ -0,0 +1,12 @@
+let date1 = new Date();
+console.log(date1);
+
+console.log(typeof date1);
+console.log(date1.toString());
+console.log(date1.toISOString());
+console.log(date1.toLocaleString());
+
+
+
+
+
