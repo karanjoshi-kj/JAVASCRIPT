6059d0565d21cb69e3a1878b2b710f49e2add418
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-05 14:24:48 +0530

    DATE & TIME

--- DateAndTime/Date.js
+++ DateAndTime/Date.js
@@ -6,6 +6,10 @@ console.log(date1.toString());
 console.log(date1.toISOString());
 console.log(date1.toLocaleString());
 
+let datetoday1 = new Date(2025,9,15,5,3,1);  // NUMBER FORMAT 
+let datetoday2 = new Date("") // INVALID DATE
+console.log(datetoday1.toLocaleString());
+console.log(datetoday2.toLocaleString());
 
 
 
