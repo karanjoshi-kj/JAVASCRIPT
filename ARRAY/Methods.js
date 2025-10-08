3c54878734fef544ba9810bc5e3f8cb5efda511b
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-08 19:09:04 +0530

    UPDATED

--- ARRAY/Methods.js
+++ ARRAY/Methods.js
@@ -1,6 +1,8 @@
 // WE WILL DISCUSS METHODS IN ARRAY . 
 let arr1  = [1,5,"karan" , 18 , "hii"];
 console.log(arr1);
-
 console.log("the size of the array is" , arr1.length);
 
+
+let arr2  = [1,10,"karan" , 18 , "hii"];
+console.log("the size of the array is" , arr2.length);
