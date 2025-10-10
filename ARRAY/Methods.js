fc2ce7e4181dac6c90d9f11e1334d1bbbd5ae5be
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-10 19:01:18 +0530

    ARRAY METHODS

--- ARRAY/Methods.js
+++ ARRAY/Methods.js
@@ -1,8 +1,18 @@
 // WE WILL DISCUSS METHODS IN ARRAY . 
-let arr1  = [1,5,"karan" , 18 , "hii"];
-console.log(arr1);
-console.log("the size of the array is" , arr1.length);
+// let arr1  = [1,5,"karan" , 18 , "hii"];
+// console.log(arr1);
+// console.log("the size of the array is" , arr1.length);
 
 
-let arr2  = [1,10,"karan" , 18 , "hii"];
-console.log("the size of the array is" , arr2.length);
+// let arr2  = [1,10,"karan" , 18 , "hii"];
+// console.log("the size of the array is" , arr2.length);
+
+let arr3 = [1,2,5,8,7]
+console.log(arr3)
+arr3.push(10);
+arr3.push(12);
+arr3.pop() // IT REMOVES THE LAST VALUE ..
+arr3.unshift(9)   // IT WILL ADD THE ELEMENT IN THE BEGINING , IT IS NOT AN OPTIMAL METHOD BECAUSE WE HAVE TO SHIFT ALL THE METHODS IN THE ARRAYT ..
+console.log(arr3);
+
+
