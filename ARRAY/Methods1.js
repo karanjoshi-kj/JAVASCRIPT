fbe35c98e9368968b9e3a32f662c8ee7a2914577
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-11 17:10:26 +0530

    METHODS IN JAVASCRIPT

--- ARRAY/Methods1.js
+++ ARRAY/Methods1.js
@@ -1,3 +1,6 @@
 arr = [5,8,9,10,12]
 console.log(arr.includes(9));  //IT RETURNS BOOLEAN DATATYPE 
 
+console.log(arr.indexOf(8));
+console.log(arr.indexOf(50));  // IF THE ELEMENT DOES'NT EXIST IT WILL RETURN -1 
+
