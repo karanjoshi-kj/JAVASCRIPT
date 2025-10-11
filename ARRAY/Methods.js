41575b5a6c64365e6c2d69744fba6de692e10a40
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-11 16:53:09 +0530

    METHODS IN JAVASCRIPT

--- ARRAY/Methods.js
+++ ARRAY/Methods.js
@@ -8,12 +8,28 @@
 // console.log("the size of the array is" , arr2.length);
 
 let arr3 = [1,2,5,8,7]
-console.log(arr3)
-arr3.push(10);
-arr3.push(12);
-arr3.pop() // IT REMOVES THE LAST VALUE ..
-arr3.unshift(9)   // IT WILL ADD THE ELEMENT IN THE BEGINING , IT IS NOT AN OPTIMAL METHOD BECAUSE WE HAVE TO SHIFT ALL THE METHODS IN THE ARRAYT ..
-arr3.shift() // REMOVES THE BEGINING VALUE ..
-console.log(arr3);
+// console.log(arr3)
+// arr3.push(10);
+arr3.push(12,23,65);
+// arr3.pop() // IT REMOVES THE LAST VALUE ..
+
+let arr4 = [2,4,9,[523,45,12,90]]  //NESTED ARRAY AS WHOLE ONE ELEMENT FOR THE OUTER ARRAY ...
+console.log(arr4.length);
+console.log(arr4[3].length);  //TO FIND THE LENGHT OF THE NESTED ARRAY ..
+
+console.log(arr4.pop);
+// POP FROM THE FIRST ELEMENT .. 
+arr4.shift();
+console.log(arr4); 
+
+//PUSH ELEMENT AT THE FIRST .. 
+arr4.unshift([11,22,33,44])
+console.log(arr4);
+
+
+
+
+
+
 
 
