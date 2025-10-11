13131b961ab0c592a39308d79373f76a2d593a19
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-11 17:34:58 +0530

    ARRAY METHODS IN JAVASCRIPT

--- ARRAY/Methods1.js
+++ ARRAY/Methods1.js
@@ -4,3 +4,9 @@ console.log(arr.includes(9));  //IT RETURNS BOOLEAN DATATYPE
 console.log(arr.indexOf(8));
 console.log(arr.indexOf(50));  // IF THE ELEMENT DOES'NT EXIST IT WILL RETURN -1 
 
+// JOIN METHOD --> HOLDS THE RESULT ON THE THIRD ARRAY .. 
+arr2 = [10.20,30]
+arr3 = arr.join(arr2)
+console.log(arr3); 
+
+
