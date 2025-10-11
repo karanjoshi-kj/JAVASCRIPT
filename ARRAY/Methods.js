331150682024130f451b16a1beaa450cbce7e830
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-11 17:06:22 +0530

    METHODS IN JAVASCRIPT

--- ARRAY/Methods.js
+++ ARRAY/Methods.js
@@ -1,17 +1,17 @@
 // WE WILL DISCUSS METHODS IN ARRAY . 
-// let arr1  = [1,5,"karan" , 18 , "hii"];
-// console.log(arr1);
-// console.log("the size of the array is" , arr1.length);
+let arr1  = [1,5,"karan" , 18 , "hii"];
+ console.log(arr1);
+ console.log("the size of the array is" , arr1.length);
 
 
-// let arr2  = [1,10,"karan" , 18 , "hii"];
-// console.log("the size of the array is" , arr2.length);
+ let arr2  = [1,10,"karan" , 18 , "hii"];
+ console.log("the size of the array is" , arr2.length);
 
 let arr3 = [1,2,5,8,7]
-// console.log(arr3)
-// arr3.push(10);
+console.log(arr3)
+arr3.push(10);
 arr3.push(12,23,65);
-// arr3.pop() // IT REMOVES THE LAST VALUE ..
+arr3.pop() // IT REMOVES THE LAST VALUE ..
 
 let arr4 = [2,4,9,[523,45,12,90]]  //NESTED ARRAY AS WHOLE ONE ELEMENT FOR THE OUTER ARRAY ...
 console.log(arr4.length);
---
+++ ARRAY/Methods1.js
@@ -0,0 +1,3 @@
+arr = [5,8,9,10,12]
+console.log(arr.includes(9));  //IT RETURNS BOOLEAN DATATYPE 
+
