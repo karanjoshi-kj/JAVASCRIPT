24775fe12268ad2ffe41f4dbe9d563ea90944b22
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-14 19:43:04 +0530

    CONSTRUCTOR METHOD OF DECLARING THE OBJECTS  IN JAVASCRIPT

---
+++ OBJECTS/ConstructorObjects.js
@@ -0,0 +1,10 @@
+// !SINGLETON OBJECT 
+// const instauser = new object()
+// console.log(instauser);
+
+// !NON-SINGLETON OBJECT  
+const instauser1 = {}
+instauser1.id = "587user"
+instauser1.name = "RAJ"
+instauser1.isLoggedin = false
+console.log(instauser1);
