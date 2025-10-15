d6227fba2f5da5f1acfeb3ff8f7e8de2f086ef2d
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-15 18:39:44 +0530

    UPDATED ON BASICS FILE

--- OBJECTS/Basics.js
+++ OBJECTS/Basics.js
@@ -2,3 +2,17 @@
 // ! THROUGH LITERALS .  --> NO FORMATION OF SINGELTON 
 // ! THROUGH CONSTRUCTORS . --> FORMATION OF SINGLETON 
 
+
+// TODO , the value of key object using array .. 
+const jsUser2 = {
+    name: "KARAN",
+    "full name":"KARAN JOSHI",
+    // [mysmb1]:"mykey1",
+    age: 18,
+    "location ARR":["RANIKHET" , "BHIMTAL" , "HALDWANI"],  //! PASSING VALUES TO KEY AS AN ARRAY
+    locationOBJ:{0:"RANIKHET" , 1:"BHIMTAL" , 2:"HALDWANI"},  //! PASSING VALUES TO KEY AS AN OBJECT 
+    email:"karan@google.com",
+    isLogged:false ,
+}
+console.log(jsUser2["location ARR"][1]);
+console.log(jsUser2.locationOBJ[2]);
