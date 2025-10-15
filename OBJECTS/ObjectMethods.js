d50e7ba002fd928ad4dbdcded159b3bdacbabff1
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-15 18:10:41 +0530

    CONSTRUCTOR METHOD OF DEFINING THE OBJECTS IN JAVASCRIPT

---
+++ OBJECTS/ObjectMethods.js
@@ -0,0 +1,8 @@
+const instauser1 = {}
+instauser1.id = "587user"
+instauser1.name = "RAJ"
+instauser1.isLoggedin = false
+
+console.log(instauser1);
+// TODO , IF WE WANT TO ACCESS THE KEYS OF THE OBJECTS 
+console.log(Object.keys(instauser1));  //! IT WILL RETURN AN ARRAY 
