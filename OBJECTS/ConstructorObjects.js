687a6ca54e93c7096faf219ef6700685d52b0b0a
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-19 11:10:11 +0530

    IMPORTANT TOPICS FOR INTERVIEW POV

--- OBJECTS/ConstructorObjects.js
+++ OBJECTS/ConstructorObjects.js
@@ -1,62 +1,78 @@
-// !SINGLETON OBJECT 
-// const instauser = new object()
-// console.log(instauser);
+// // // !SINGLETON OBJECT 
+// // // const instauser = new object()
+// // // console.log(instauser);
 
-// !NON-SINGLETON OBJECT  
-const instauser1 = {}
-instauser1.id = "587user"
-instauser1.name = "RAJ"
-instauser1.isLoggedin = false
-// console.log(instauser1);
-
-
-const regularuser = {
-     email:"karanjoshi@goggle.com",
-     fullname:{
-        userfullname:{
-            firstname:"karan",
-            lastname:"joshi"
-        }
-     }
-}
-// ?ACCESSING OF NESTED OBJECCTS
-console.log(regularuser.fullname.userfullname.firstname);
-``
-// TODO , MERGING OF THE TWO OBJECTS......
-obj1 = {1 : "a" , 2 : "b"}
-obj2 = {3 : "c" , 4 : "d"}
-// ?METHOD 1 :const obj3 = Object.assign(obj1 , obj2)
-// ?METHOD 2 --> IT IS GOOD METHOD FOR MERGING OF THE OBJECTS IN JAVASCIRPT BCOZZ THIS IS AN OPTIONAL PARAMETER WHICH ACTS AS TARGET FOR OTHER SOURCE ....  
-// const obj3 = Object.assign({} , obj1 , obj2) //! OBJECT ASSIGN IS A STATIC METHOD .. 
-console.log(obj3);
-// TODO , BUT WE DON'T USE THESE METHODS ON INDUSTRIES , WE USE SPREADING CONCEPT 
-const obj3 = {...obj1 , ...obj2}
-// console.log(obj3);
+// // // !NON-SINGLETON OBJECT  
+// // const instauser1 = {}
+// // instauser1.id = "587user"
+// // instauser1.name = "RAJ"
+// // instauser1.isLoggedin = false
+// // // console.log(instauser1);
 
 
-// TODO , WHEN VALUES COMES FROM THE DATABSES WE USE ARRAY OF OBJECS .. 
+// // const regularuser = {
+// //      email:"karanjoshi@goggle.com",
+// //      fullname:{
+// //         userfullname:{
+// //             firstname:"karan",
+// //             lastname:"joshi"
+// //         }
+// //      }
+// // }
+// // // ?ACCESSING OF NESTED OBJECCTS
+// // console.log(regularuser.fullname.userfullname.firstname);
+// // ``
+// // // TODO , MERGING OF THE TWO OBJECTS......
+// // obj1 = {1 : "a" , 2 : "b"}
+// // obj2 = {3 : "c" , 4 : "d"}
+// // // ?METHOD 1 :const obj3 = Object.assign(obj1 , obj2)
+// // // ?METHOD 2 --> IT IS GOOD METHOD FOR MERGING OF THE OBJECTS IN JAVASCIRPT BCOZZ THIS IS AN OPTIONAL PARAMETER WHICH ACTS AS TARGET FOR OTHER SOURCE ....  
+// // // const obj3 = Object.assign({} , obj1 , obj2) //! OBJECT ASSIGN IS A STATIC METHOD .. 
+// // console.log(obj3);
+// // // TODO , BUT WE DON'T USE THESE METHODS ON INDUSTRIES , WE USE SPREADING CONCEPT 
+// // const obj3 = {...obj1 , ...obj2}
+// // // console.log(obj3);
 
-const users = [
-    {
-        id:1,
-        email:"karan@gmail.com"
-    },
-    {
-        id:2,
-        email:"karan@gmail.com"
-    },                                 
-    {
-        id:3,
-        email:"karan@gmail.com"
-    },
-]
 
-users[1].email
-console.log(regularuseruser);
+// // // TODO , WHEN VALUES COMES FROM THE DATABSES WE USE ARRAY OF OBJECS .. 
+
+// // const users = [
+// //     {
+// //         id:1,
+// //         email:"karan@gmail.com"
+// //     },
+// //     {
+// //         id:2,
+// //         email:"karan@gmail.com"
+// //     },                                 
+// //     {
+// //         id:3,
+// //         email:"karan@gmail.com"
+// //     },
+// // ]
+
+// // users[1].email
+// // console.log(regularuseruser);
 
 
-// !INTERVIEW TOPICS :  
-obj1 = {"full-name" : "karan" , "age" : 19},
-obj2 = {"full-name" : "karan" , "age" : 19}
-obj3 = {...obj1 , obj2}  // TODO , NOW OBJ1 ARE DIRECT KEY VALUE PAIR FOR OBJ3 & WE DON'T SPREAD THE OBJ2 SO OBJ2 IS KEY FOR OBJ1 AND ITS CURLY BARCES ELEMENTS ARE IT'S VALUES         
-console.log(obj3);
+// // // !INTERVIEW TOPICS :  
+// // obj1 = {"full-name" : "karan" , "age" : 19},
+// // obj2 = {"full-name" : "karan" , "age" : 19}
+// // obj3 = {...obj1 , obj2}  // TODO , NOW OBJ1 ARE DIRECT KEY VALUE PAIR FOR OBJ3 & WE DON'T SPREAD THE OBJ2 SO OBJ2 IS KEY FOR OBJ1 AND ITS CURLY BARCES ELEMENTS ARE IT'S VALUES         
+// // console.log(obj3);
+
+
+// obj1 = {"full-name" : "karan" , "age" : 19},
+// obj2 = {"full-name" : "karan" , "age" : 19}
+// console.log(obj1 , obj2); //TODO , BOTH HAVE SAME VALUES BUT NOT SAME REFERENCE MEANS EACH WILL CREATE A NEW OBJECT SO BOTH WILL CREATE DIFFERENT MEMORY LOCATIONS  
+
+// obj1 = {"full-name" : "karan" , "age" : 19};
+// let obj2 = obj1; //! NOW OBJECT2 IS A SHALLOW COPY(SAME REFERNCE)
+// console.log((obj1 === obj2)); //?TRUE 
+// console.log(obj1 , obj2);
+
+let obj1 = {"full-name" : "karan" , "age" : 19};
+let obj2 = {"full-name" : "karan" , "age" : 19};
+
+let arr = [obj1 , obj2]
+console.log(arr[0]); //!PRINTS IT SINGLE TIME ..
