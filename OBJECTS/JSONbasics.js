7f18bc3510a13f411f8effd1a2b764ec37dd0b11
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-19 11:17:52 +0530

    JSON CONCEPT IN BRIEF

---
+++ OBJECTS/JSONbasics.js
@@ -0,0 +1,7 @@
+// TODO , JSON -- > JAVASCRIPT OBJECT NOTATION 
+// !JSON is used to store or send data,
+// !and it always converts everything into a string format
+
+let obj = { name: "Karan", age: 19 };
+console.log(obj); //? IW WILL READ KEYS AS "name" , "age"
+
