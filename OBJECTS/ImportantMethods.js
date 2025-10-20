d243dad2e730d0f7809c907f63f18ae8f5bbd68f
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-20 07:51:22 +0530

    IMPORTANT METHODS IN JAVASCRIPT

---
+++ OBJECTS/ImportantMethods.js
@@ -0,0 +1,9 @@
+obj = {
+    name:"karan",
+    course:"btech",
+    age:19
+}
+console.log(Object.keys(obj)); //!return in array format 
+console.log(Object.values(obj)); //!return in array format 
+
+
