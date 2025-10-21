b425f98773f0e6fe75c36e5100cfb1efddf1f5d6
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-21 06:05:39 +0530

    FUNCTIONS IN JAVASCRIPT

---
+++ FUNCTIONS/Basics.js
@@ -0,0 +1,8 @@
+// TODO , ADDINGOF TWO NUMBERS  . 
+
+function addtwonumbers(number1 , number2) { //?in THE FUNCTION WE PASS THE PARAMETERS
+    console.log(number1 + number2);  //! SCOPE OF THE FUNCTION .. 
+}
+// ?addtwonumbers --> THIS IS ONLY THE REFERAL OF THE FUNCTION , MEANS IT TELLS THAT THE FUNCTION EXIST HERE.. 
+addtwonumbers(5,5); //? CALLING OF THE FUNCTION
+//? DURING THE CAALING OF THE FUNCTON WE PASS THE ARGUMENTS .. 
