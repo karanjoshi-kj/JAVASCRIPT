2cab75895a28e1e495ddeade3e73ea5955d382a2
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-21 06:16:44 +0530

    FUNCTIONS IN JAVASCRIPT

--- FUNCTIONS/Basics.js
+++ FUNCTIONS/Basics.js
@@ -1,8 +1,21 @@
-// TODO , ADDINGOF TWO NUMBERS  . 
+// TODO , ADDING OF TWO NUMBERS  . 
+
+// function addtwonumbers(number1 , number2) { //?in THE FUNCTION WE PASS THE PARAMETERS
+//     console.log(number1 + number2);  //! SCOPE OF THE FUNCTION .. 
+// }
+// ?addtwonumbers --> THIS IS ONLY THE REFERAL OF THE FUNCTION , MEANS IT TELLS THAT THE FUNCTION EXIST HERE.. 
+// addtwonumbers(5,5); //? CALLING OF THE FUNCTION
+//? DURING THE CAALING OF THE FUNCTON WE PASS THE ARGUMENTS .. 
+
+// TODO , RETURNING OF THE FUNCTION 
 
 function addtwonumbers(number1 , number2) { //?in THE FUNCTION WE PASS THE PARAMETERS
-    console.log(number1 + number2);  //! SCOPE OF THE FUNCTION .. 
+    let result = number1 + number2;  //?STORING THE VALUES IN THE RESULT VARIABLE ..
+    return result;//?RETURN RESULT ..
+    console.log("karan");
+    
 }
-// ?addtwonumbers --> THIS IS ONLY THE REFERAL OF THE FUNCTION , MEANS IT TELLS THAT THE FUNCTION EXIST HERE.. 
-addtwonumbers(5,5); //? CALLING OF THE FUNCTION
-//? DURING THE CAALING OF THE FUNCTON WE PASS THE ARGUMENTS .. 
+const result = addtwonumbers(5,5)
+console.log("RESULT:" , result);
+
+//!NOTE : NO STATEMENT OR VALUES ARE NOT ALLOWED TO PRINT AFTER RETURNING OF THE FUNCTION .. 
