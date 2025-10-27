aaeaa0a8dba3bba963c0893a2e2e246ade36a3bf
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-27 17:37:57 +0530

    FORMAL &  ACTUAL ARGUMENTS

--- FUNCTIONS/Logics.js
+++ FUNCTIONS/Logics.js
@@ -1,8 +1,16 @@
 // TODO , FORMAL ARGUMENTS --> PARAMETERS WRITTEN INSIDE THE FUNCTION DEFINATION . 
 // TODO , ACTUAL ARGUMENTS --> PARAMETERS PASSED WHEN THE FUNCTION CALL . 
 
-function add(a , b) {
+// function add(a , b) {
+//     console.log(a+b);
+//     return a+b ;
+// }
+// add(5,5);     
+
+function add(a,b) {
     console.log(a+b);
-    return a+b ;
 }
-add(5,5);
+let x = 10;
+let y = 10;
+
+add(x,y)   // ! HERE THE VALUE OF X IS COPIED TO A AND VALUE OF Y IS COPIED TO B 
