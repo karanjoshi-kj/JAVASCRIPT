8a25625ac6060792be37909f915fd5728e27ba23
karan joshi <kjkaranjoshi2006@gmail.com>
2025-10-29 18:56:19 +0530

    SCOPES BASICS AND IT'S METHOD

--- SCOPES/Method.js
+++ SCOPES/Method.js
@@ -9,3 +9,14 @@ console.log(user.username);
 user.printname();
 
 
+//! NOW IF I HAD CHANGE THE OBJECT NAME & MULTIPLE METHODS INSIDE N OBJECT THEN WE HAVE TO CHANGE THE OBJECT NAME IN EVERY FUNCTION THEN HERE THE COMPLEXITY INCREASES , SO WE USE THE THIS KEYWORD .. 
+
+const userdata = {
+    username : "karan",
+    age : 19,
+    printname(){   //? NO NEED TO WRITE THE FUNCTION BEFORE THE FUNCTION NAME BCOZZ OBJECTS ARE KEY-VALUE PAIR & FUNCTION NAME ACT AS KEY AND IT'S DEFINATION ACT AS VALUES .. 
+        console.log(this.username);
+    }
+}
+console.log(userdata.username);
+userdata.printname();
