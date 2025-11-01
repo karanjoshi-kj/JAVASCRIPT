e33154e317407cda596d355fbc8b4cb13ace1727
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-01 17:48:50 +0530

    QUESTION PRACTICE ON FUNCTIONS

---
+++ FUNCTIONS/Question.js
@@ -0,0 +1,12 @@
+// TODO , CHECK THE PARAMETER A IS AN ARRAY .
+
+a1 = [10,20,30,40]
+function printval(a) {
+    if (Array.isArray(a)) {
+        console.log(a);
+    }
+    else{
+        console.log("ERROR");
+        
+    }      
+}
