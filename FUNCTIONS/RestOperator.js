90d1de8b7c69594edb807e8a04a13c675135a587
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-02 11:37:47 +0530

    QUESTIONS PRACTICE ON FUNCTIONS

---
+++ FUNCTIONS/RestOperator.js
@@ -0,0 +1,6 @@
+function showdetails(name , ...hobbies) {
+    console.log("NAME" , name);
+    console.log("HOBBIES" , hobbies);     // TODO , ALL THE VAUES WILL GO ON THE HOBBIES .. 
+
+}
+showdetails("karan" , "coding" , "gaming" , "music")
