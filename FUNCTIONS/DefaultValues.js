91950c4a113176563e6c36bfc061c319f5a2e74a
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-02 11:45:51 +0530

    QUESTIONS PRACTICE ON FUNCTIONS

--- FUNCTIONS/DefaultValues.js
+++ FUNCTIONS/DefaultValues.js
@@ -4,3 +4,10 @@ function greet(name = "Guest") {
 greet("KARAN");
 greet();
 
+
+function add(a = 5 , b = 10) {
+    return a+b;
+}
+console.log(add(2,3));
+console.log(add(18));
+console.log(add());
