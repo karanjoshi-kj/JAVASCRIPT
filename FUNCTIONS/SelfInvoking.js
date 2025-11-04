c080296648debd4997d52282b9a9458342226653
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-04 18:20:16 +0530

    SELF INVOKING FUNCTION

--- FUNCTIONS/SelfInvoking.js
+++ FUNCTIONS/SelfInvoking.js
@@ -1,5 +1,22 @@
 // TODO , THE FUNCTION DID'NT REQIURE EXTERNAL CALLING IS CALLED SELF INVOKING FUNCTION . 
+// TODO , IT IS ALSO KNOWN AS ANONYMS FUNCTION . 
+
 
 (function selfinvoking() {
     console.log("RADHE RADHE");
 })();
+
+
+
+(function selfinvoking() {
+    const date = new Date().toString();
+    console.log("INDIAN STANDARD TIMING");
+    console.log(date);
+})();
+
+
+// !,WE CAN ALSO PASS ARGUMENTS IN THE SELF INVOKING FUNCTION . 
+(function selfinvoking(...a) {
+      console.log(...a);
+      
+})(10,20,30,40,50);
