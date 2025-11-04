fbe83aba0efd3ac818f04e4cd1813ad578a35884
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-04 18:27:07 +0530

    SELF INVOKING FUNCTION

--- FUNCTIONS/Question2.js
+++ FUNCTIONS/Question2.js
@@ -1,3 +1,7 @@
-function showdetails(name , ) {
+function show(cls , branch , ...hobbies) {
+    console.log("year" , cls);
+    console.log("BRANCH" , branch);
+    console.log("HOBBIES" , hobbies);
     
 }
+show("3rd" , "CSE" , "music" , "horse riding" , "travelling")
