ed2c46835d136d3655fcd4d169bcff9441b35dc7
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-07 19:28:18 +0530

    FAT ARROW FUNCTION

--- FUNCTIONS/FatarrowFunction.js
+++ FUNCTIONS/FatarrowFunction.js
@@ -18,3 +18,14 @@ console.log("THE ADDITION OF TWO NUMBERS IS:" , add(10,20));
 
 
 //! NOTE : FATAARROW FUNCTION IS ALWAYS USED FOR SINGLE LINER FUNCTIONS . 
+
+//?EXAMPLE 3:
+const add = (a,b) => a+b;  //? HERE THE a+b WILL ACT AS RETURN STATEMENT . 
+console.log(add(10,10));
+
+//?EXAMPLE 4:
+const add = (a,b) => {
+     console.log("HELLO WORLD");  
+     return a+b;  //? HERE IT WILL NOT PRINT THE RETURN STATEMENT BCOZZ IT IS USED FOR SINGLE LINER STATEMENT . 
+}
+add(10,20)
