8935379753b2da587b34c4e951283886f9dc5a9b
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-07 19:07:57 +0530

    FUNCTION REFERENCING IN JS

---
+++ FUNCTIONS/FunctionReferencing.js
@@ -0,0 +1,6 @@
+//TODO, WHEN WE PASS THE WHOLE FUNCTION TO AN VARIABLE , THIS IS KNOWN AS FUNCTION REFERENCING .  
+
+var printhello = function hello(a){
+    console.log("HELLO WORD" ,a);
+}
+printhello(10)
