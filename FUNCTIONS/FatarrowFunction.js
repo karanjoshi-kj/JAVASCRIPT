698f551e27f17186ff2bbf6e4e3610d48e8eb406
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-07 19:18:55 +0530

    FAT ARROW FUNCTION

---
+++ FUNCTIONS/FatarrowFunction.js
@@ -0,0 +1,20 @@
+// todo , FAT ARROW FUNCTION --> ARROW FUNCTION -->ES6 ECMA Script
+
+//  todo ,  a fat arrow function is always defined using const keyword .
+// todo , in fat arrow function we does'nt provide the name of function , only variable name is required .
+// todo , it is denoted by => . 
+
+//? EXAMPLE 1:
+const printhello = () => {
+    console.log("radhe radhe");
+}
+printhello();
+
+//? EXAMPLE 2: FUNCTION REFERENCING  
+const add = function addition(a,b) {
+    return a+b;
+}
+console.log("THE ADDITION OF TWO NUMBERS IS:" , add(10,20));
+
+
+//! NOTE : FATAARROW FUNCTION IS ALWAYS USED FOR SINGLE LINER FUNCTIONS . 
