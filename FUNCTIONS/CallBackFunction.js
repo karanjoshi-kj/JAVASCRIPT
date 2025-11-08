29f53016603eba39f93293bddfd70a8fac5097d9
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-08 12:39:44 +0530

    CALL BACK FUNCTION

--- FUNCTIONS/CallBackFunction.js
+++ FUNCTIONS/CallBackFunction.js
@@ -7,3 +7,13 @@ function printhello(name) {
     console.log(name());
 }
 printhello(username)  //? HERE USERNAME IS ACTUAL ARGUMENT WHICH MAKES A COPY OF THE RETURN STATEMENT ON THE NAME ARGUMENT . 
+
+// ?example 2:
+const add = function (a,b,callback) {
+    return callback(a,b)
+}
+console.log(
+    add(5,5,function(n1,n2){
+        return n1+n2
+    })
+);
