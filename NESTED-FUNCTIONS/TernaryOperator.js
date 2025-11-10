3611b014900892e9e6a49396d4f6322d408290c0
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-10 18:57:08 +0530

    TERNARY OPERATOR

--- NESTED-FUNCTIONS/TernaryOperator.js
+++ NESTED-FUNCTIONS/TernaryOperator.js
@@ -4,3 +4,10 @@
 let age = 18;
 let result = (age >=18) ? "YOU CAN VOTE" : "YOU CANNOT VOTE" ;
 console.log(result);
+
+// !QUESTION 1: --> TO CHECK A NUMBER IS EVEN OR NOT 
+
+let num = 7;
+let ans = (num % 2 == 0) ? "THE NUMBER IS EVEN" : "THE NUMBER IS ODD";
+console.log(ans);
+
