66209dfad8446ae1119235fa28ca1ae6f3f1d163
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-11 19:24:41 +0530

    PALINDROME

--- CONTROLFLOW/Palindrome.js
+++ CONTROLFLOW/Palindrome.js
@@ -1,14 +1,25 @@
+// for(let i=1 ; i<=50 ;i++){
+//     let temp = i;  //? number store kar liya
+//     let rev = 0;  //? reverse number
+//     let num = i;  //?original number store 
+
+//     while (temp>0) {
+//         let digit = temp%10;
+//         rev = (rev*10)+digit
+
+//         temp = Math.floor(temp / 10)
+//     }
+//     if (num===rev) 
+//         console.log(num);
+// }
+
 for(let i=1 ; i<=50 ;i++){
-    let temp = i;  //? number store kar liya
-    let rev = 0;  //? reverse number
-    let num = i;  //?original number store 
-
-    while (temp>0) {
-        let digit = temp%10;
-        rev = (rev*10)+digit
-
-        temp = Math.floor(temp / 10)
+    let rev = 0;
+    while (i>0) {
+        let digit = i%10   //?LAST DIGIT OF THE NUMBER
+        rev = rev *10 + digit  //? REVERSE OF THE NUMBER 
+        i = Math.floor(i/10)  //? REMOVE THE LAST DIGIT 
     }
-    if (num===rev) 
-        console.log(num);
+    if(i === rev)
+        console.log(i);
 }
