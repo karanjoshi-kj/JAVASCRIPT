192f5e5bd2c6981ba20ecab709eb11e244c80b26
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-16 11:21:59 +0530

    PATTERNS IN JS

--- CONTROLFLOW/Pattern4.js
+++ CONTROLFLOW/Pattern4.js
@@ -1,7 +1,7 @@
 
 for (let i = 1; i <= 5 ; i++) {
     let line = "";
-    for(let j = 1 ; j<= 5; j++){
+    for(let j = 1 ; j<= 5 ; j++){
         if(j <= 5-i)
             line += " ";   //? THIS IS FOR SPACES .. 
         else
