412da515a7d01322f47923ba0e57db384da9041d
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-16 11:13:26 +0530

    PATTERNS IN JS

---
+++ CONTROLFLOW/Pattern4.js
@@ -0,0 +1,11 @@
+
+for (let i = 1; i <= 5 ; i++) {
+    let line = "";
+    for(let j = 1 ; j<= 5; j++){
+        if(j <= 5-i)
+            line += " ";   //? THIS IS FOR SPACES .. 
+        else
+            line += "*"
+    }
+    console.log(line);
+}
