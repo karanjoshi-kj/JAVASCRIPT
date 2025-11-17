084002a82c605aaf8516036edf076ad194c314dc
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-17 19:11:54 +0530

    SPECIAL LOOPS IN JS

--- SPECIAL-LOOPS/ForEach1.js
+++ SPECIAL-LOOPS/ForEach1.js
@@ -6,19 +6,35 @@
 
 // TODO , FOR-OF -->  DIRECT ITERATION OVER ARRAY ..
 
-arr = [10,20,"$",50,80];  //?JAVASCRIPT TREAT ARRAY AS AN OBJECT . 
-for (const i of arr) {
+// arr = [10,20,"$",50,80];  //?JAVASCRIPT TREAT ARRAY AS AN OBJECT . 
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+
+
+// // TODO , FOR-OF -->  DIRECT ITERATION OVER STRING ..
+
+// str = "kjkaranjoshi2006@gmail.com"
+// for (const karan of str) {
+//     console.log(karan);
+//     //?HERE KARAN IS ONLY AN ITERATOR OVER STRING STR .. 
+// }
+
+
+// TODO , FOR-OF -->  DIRECT ITERATION OVER OBJECT ..
+
+
+// TODO ,FOR OF LOOP IS NOT DIRECTLY ITERTABLE ON THE OBJECT .. 
+obj = {
+    name : "karan",
+    colleg : "BIAS",
+    age : 19,
+};
+
+for (const i of Object.keys(obj)) {  //? THIS WILL PRINT KEY-PAIRS ..  
     console.log(i);
 }
 
-
-
-// TODO , FOR-OF -->  DIRECT ITERATION OVER STRING ..
-
-str = "kjkaranjoshi2006@gmail.com"
-for (const karan of str) {
-    console.log(karan);
-    
-}
 
 
