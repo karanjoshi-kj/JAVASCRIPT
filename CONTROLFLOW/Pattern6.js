7fadc2f06a6eb0637156cc23757e849c177eae53
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-17 18:15:04 +0530

    UPDATED

--- CONTROLFLOW/Pattern6.js
+++ CONTROLFLOW/Pattern6.js
@@ -1,15 +1,12 @@
 let start = 2;
-
 for(let i = 1 ; i<= 10 ; i++){
     let num = start
     let line = "";
-
     for(j = 1; j<=i ; j++){
         line += num + " ";
         num += 2;
     }
     console.log(line);
     start++
-    
 }
 
