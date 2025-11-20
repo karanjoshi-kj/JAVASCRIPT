bc3364cf5c8b61aca055efb273ca1128da9ab784
karan joshi <kjkaranjoshi2006@gmail.com>
2025-11-20 18:11:11 +0530

    FOR IN LOOP

---
+++ SPECIAL-LOOPS/Forin.js
@@ -0,0 +1,42 @@
+
+
+// for in loop
+// for in loop abstraclty work on the indices of the object
+
+a1 = ["karan", "Shubham", 10, 99, 43, 4, 5, 66];
+// a1[0],a1[1]
+
+for (const i of a1) {
+  //if (i % 2 == 0) console.log(i); //here it  will mod the acutal elements of the array becuae FOR OF loop works on the actual element so the arrya
+}
+
+a1 = ["karan", "Shubham", 10, 99, 43, 4, 5, 66];
+// a1[0],a1[1]
+for (const i in a1) {
+  //console.log(i); // will only print the indcies of the Array
+}
+
+for (const i in a1) {
+  // console.log(a1[i]); // will print the acutal value of the array becuase we are accessing it thorugh its array index
+}
+
+for (const i in a1) {
+  //  if (i % 2 == 0) console.log(a1[i]); // here it will mod the index values of the array as the FOR IN loop focuses on the indixes of the Array
+}
+
+a1 = [1, 2, 3, [1, 2, 33, [11, 12, 13, 14]]]; //indices
+
+obj = {
+  name: "Karan", //keys are the actuals idices in the Object
+  course: "Btech",
+};
+
+for (const i in obj) {
+  //FOR IN LOOP is direclty iterable over the keys of objects
+  console.log(`${i}-->${obj[i]}`); //name-->Karan
+}
+// for (const key in a1) {
+//   for (const key1 in a1) {
+//     console.log(key);
+//   }
+// }
