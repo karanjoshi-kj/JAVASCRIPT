45460fff84f82d661b513f584e0187885e6bee2c
karan joshi <kjkaranjoshi2006@gmail.com>
2025-12-02 19:04:29 +0530

    UPDATED

---
+++ MAPS/MapsBasic.js
@@ -0,0 +1,12 @@
+// TODO  MAPS ARE THE OBJECT THAT HOLDS THE KEY VALUE PAIR WITH NO DUPLICATE VALUES . 
+
+
+const map = new Map();
+map.set('IN',"India")
+map.set('USA',"United states of america")
+map.set('FR',"French")
+map.set('IN',"India")   //? THIS KEY VALUE PAIR WILL NT PRINT OVER THE SCREEN BCOZZ MAPS DO NOT PRINT THE DUPLICATE KEY VALUE PAIRS . 
+
+
+console.log(map);
+
