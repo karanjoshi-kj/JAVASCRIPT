24308e5b5cc8e1df35c74650b5c8dfe19229d7b1
karan joshi <kjkaranjoshi2006@gmail.com>
2025-12-05 20:56:39 +0530

    QUESTION PRACTICE

---
+++ MAPS/Qusetion8.js
@@ -0,0 +1,13 @@
+// ! TYPECHECKING 
+
+const usercheck = ({name , age , email}) =>
+
+   (typeof name === "string" && name.length >= 3) && (typeof age === "number" && age >= 18) && (typeof email === "string" && email && email.includes("@") && email.includes("."));
+
+const user = {
+  name: "ram",
+  email: "kjkaranjoshi2006@gmail.com",
+  age: 19,
+};
+
+console.log(usercheck(user));
