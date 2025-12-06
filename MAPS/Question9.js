bcd7f977ef78e1f16ad37597f296fceaa664cb91
karan joshi <kjkaranjoshi2006@gmail.com>
2025-12-06 20:32:43 +0530

    QUESTION PRACTIECE

--- MAPS/Question9.js
+++ MAPS/Question9.js
@@ -1,12 +1,30 @@
-//! RANDOM OTP GENERATOR 
+// //! RANDOM OTP GENERATOR 
 
-function generateotp(length){
+// function generateotp(length){
+//     let otp = " ";
+//     for (let i = 0; i < length; i++) {
+        
+//         otp += Math.floor(Math.random() * 10) 
+//     }
+//     return otp;
+// }
+// console.log(generateotp(4));
+// console.log(generateotp(5));
+
+
+//? USING WHILE LOOP . 
+
+const generateotp = (length) => {
     let otp = " ";
-    for (let i = 0; i < length; i++) {
-
+    let i = 0;
+    
+    while (i < length) {
+        
         otp += Math.floor(Math.random() * 10) 
+        i++;
     }
-    return otp;
-}
+    return otp
+    
+};
 console.log(generateotp(4));
 console.log(generateotp(5));
